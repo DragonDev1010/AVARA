337bd30d059c138ea0ae10fd19927ae91daf535f
dragonyu1010 <provic0099@gmail.com>
2022-01-27 16:45:14 +0800

    add OwnershipModule.sol

---
+++ contracts/OwnershipModule.sol
@@ -0,0 +1,34 @@
+/**
+    ***********************************************************
+    * Copyright (c) Avara Dev. 2021. (Telegram: @avara_cc)  *
+    ***********************************************************
+
+     ▄▄▄·  ▌ ▐· ▄▄▄· ▄▄▄   ▄▄▄·
+    ▐█ ▀█ ▪█·█▌▐█ ▀█ ▀▄ █·▐█ ▀█
+    ▄█▀▀█ ▐█▐█•▄█▀▀█ ▐▀▀▄ ▄█▀▀█
+    ▐█ ▪▐▌ ███ ▐█ ▪▐▌▐█•█▌▐█ ▪▐▌
+     ▀  ▀ . ▀   ▀  ▀ .▀  ▀ ▀  ▀  - Binance Smart Chain
+
+    Avara - Always Vivid, Always Rising Above
+    https://avara.cc/
+    https://github.com/avara-cc
+    https://github.com/avara-cc/Avara/wiki
+*/
+
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity 0.8.4;
+
+import "./abstract/AvaraModule.sol";
+import "./library/SafeMath.sol";
+
+contract OwnershipModule is AvaraModule {
+    constructor(address cOwner, address baseContract) AvaraModule(cOwner, baseContract, "Ownership", "0.0.1") {}
+
+    /**
+    * @dev Occasionally called (only) by the server to make sure that the connection with the module and main contract is granted.
+    */
+    function ping() external view onlyOwner returns (string memory) {
+        return getBaseContract().ping();
+    }
+}
