e9b3f954c30080337815bbdd77e192c5103d651e
dragonyu1010 <provic0099@gmail.com>
2022-01-27 23:24:18 +0800

    fork bsc main net and add ownershipModule into unit testing

--- test/Test.js
+++ test/Test.js
@@ -5,9 +5,11 @@ require('chai')
 const {assert} = require('chai')
 
 const AvaraNft = artifacts.require("AvaraNft");
+const OwnershipModule = artifacts.require("OwnershipModule")
 
 contract('Marketplace Contract', (accounts) => {
     before(async() => {
         avaraNft = await AvaraNft.deployed()
+        ownershipModule = await OwnershipModule.deployed()
     })
 })
